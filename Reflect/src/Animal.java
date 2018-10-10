8562df3946c7c32df0d3b8d36d78eeb7b6ce8cdc
Tamri <Tamri@160.97.168.130>
2018-10-10 14:12:56 +0200

    initial commit of EGIT

---
+++ Reflect/src/Animal.java
@@ -0,0 +1,20 @@
+import com.sun.org.apache.bcel.internal.classfile.Method;
+
+public class Animal {
+
+	public Animal(String string, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Class c = new Animal("Dog",9).getClass();
+//		Method[] m = c.getMethods();
+//		System.out.println("there are" + m.lengthe + "methods in this class");
+//			
+//			for(int i=0;i < m.length; i++) {
+//				System.out.println(m[i]);
+//			}
+	}
+
+}
---
+++ Reflect/src/app.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(app.class.getMethods());  //[Ljava.lang.reflect.Method;@4e25154f
+
+System.out.println(app.class.getName()); //app
+  
+//for(int x=0; x<5; x++) {
+//	System.out.println(Array.getLength(x));	 
+//}
+
+	
+ 
+	}
+
+}
